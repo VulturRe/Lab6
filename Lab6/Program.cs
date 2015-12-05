d03b301c2e1c6563b8c97b87383500b21d2ce5f8
Ilshat Galimov <einnks@gmail.com>
2015-12-05 22:20:48 +0300

    Lab6 is done

---
+++ Lab6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = @"c:\users\ilshat\test.txt";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файла с заданным именем не существует.");
+                Console.ReadKey();
+                return;
+            }
+
+            using (var sr = File.OpenText(path))
+            {
+                string s;
+                var stack = new Stack<string>();
+                while ((s = sr.ReadLine()) != null)
+                    stack.Push(s);
+                foreach (var str in stack)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
